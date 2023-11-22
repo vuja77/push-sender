3255624c67c4db2047dccfe4bc0d31cece7a6489
Vuja <djordjijevujovic210306@gmail.com>
2023-11-22 18:02:43 +0100

    fix 2

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
   }
   useEffect(() => {
     fetchLastNews()
-  })
+  }, [])
   return (
     <div className="App">
       <button onClick={() => Send()}>Send notification</button>
